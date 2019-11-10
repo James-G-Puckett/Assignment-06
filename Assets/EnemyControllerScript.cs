cf5b9396f2c4e19e2b2642b65c79f79d4a802d02
James-G-Puckett <54376462+James-G-Puckett@users.noreply.github.com>
2019-11-09 23:18:27 -0600

    Initial commit

---
+++ Assets/EnemyControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+
+public class EnemyControllerScript : MonoBehaviour
+{
+    public NavMeshAgent enemyAgent;
+    public GameObject player;
+    public static int timesKilled = 0;
+    public Text killText;
+    //Start is called before the first frame update
+    void Start()
+    {
+        enemyAgent.updateRotation = false;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyAgent.SetDestination(player.transform.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            timesKilled += 1;
+            killText.text = "Caught: " + timesKilled;
+        }
+    }
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    public GameObject player;
+    public static int targetsDestroyed = 0;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        scoreText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            targetsDestroyed += 1;
+            scoreText.text = "Score: " + targetsDestroyed;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
